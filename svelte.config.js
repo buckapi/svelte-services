4b5476d4cdd8f4161af56228c16d8c9a8fa3b2ab
root <root@localhost.localdomain>
2024-12-09 09:34:27 -0500

    limit increm

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-// import adapter from '@sveltejs/adapter-static';
 
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	plugins: [sveltekit()],
-	resolve: {
-	  alias: {
-		$lib: '/src/lib', // Define correctamente la ruta a tu carpeta lib
-	  },
-	},
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    alias: {
+      $lib: './src/lib', // Ejemplo de alias
+      $components: './src/components'
+    },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    })
+  }
 };
-
-export default config;
